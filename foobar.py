08af42cb2a240d55b626f714cec0ac4892869085
Mrxpxpb <dennis01102001@gmail.com>
2020-10-20 09:00:59 +0200

    Optimised foobar code

--- foobar.py
+++ foobar.py
@@ -1,13 +1,11 @@
-numbers = range(0,100)
-output = ''
-for number in numbers:
+for number in range(1, 100):
+    output = ''
     if number % 2 == 0:
         output += 'foo'
     if number % 3 == 0:
-        output += 'bar'
-    
+        output += 'bar'    
     if output == '':
         output = str(number)
-    print(output + '\n') 
-    output = ''
+    print(output) 
+    
  
